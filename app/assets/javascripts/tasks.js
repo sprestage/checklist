a4e715d5ec523a0d2df0204d0b8439a88f319df3
Susan Prestage <susan.prestage@gmail.com>
2013-11-17 18:44:07 -0800

    fixed the imcomplete_tasks bug

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,11 +1,20 @@
-jQuery.fn.submitOnCheck = function() {
+$(function () {
+  $('.edit_task input[type=submit]').remove();
+  $('.edit_task input[type=checkbox]').click(function () {
+    $(this).parent('form').submit();
+  });
+});
+
+$(function () {
+  $('.edit_task').submitOnCheck();
+});
+
+jQuery.fn.submitOnCheck = function () {
   this.find('input[type=submit]').remove();
-  this.find('input[type=checkbox]').click(function() {
+  this.find('input[type=checkbox]').click(function () {
     $(this).parent('form').submit();
   });
   return this;
 };
 
-$(function() {
-  $('.edit_task').submitOnCheck();
-});
+
